5c92054929f513d3a5d141b020ff5a72a75866cd
Mccc <mancong@bertadata.com>
2023-11-22 17:42:25 +0800

    新增演示用例

--- Example/SmartCodable/ViewController.swift
+++ Example/SmartCodable/ViewController.swift
@@ -120,6 +120,7 @@ extension ViewController {
             "list": [
                 ["name": "扁平化", "vc": "CaseOneViewController"],
                 ["name": "派生关系", "vc": "CaseTwoViewController"],
+                ["name": "多值映射", "vc": "CaseThreeViewController"],
             ]
         ]
     }
