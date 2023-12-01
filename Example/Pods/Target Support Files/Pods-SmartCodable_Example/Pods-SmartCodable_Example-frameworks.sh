5394b878eb86f2603c7cd61b84b4d88b6940a7cd
Mccc <mancong@bertadata.com>
2023-12-01 18:30:47 +0800

    新增解析date和data的能力

--- Example/Pods/Target Support Files/Pods-SmartCodable_Example/Pods-SmartCodable_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-SmartCodable_Example/Pods-SmartCodable_Example-frameworks.sh
@@ -176,12 +176,10 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SmartCodable/SmartCodable.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SmartCodable/SmartCodable.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
