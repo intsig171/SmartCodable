df277adbaecb29969631c48dcfd4ae3d274b5f0f
Mccc <mancong@bertadata.com>
2024-03-01 14:40:43 +0800

    数组嵌套字典

--- Example/SmartCodable/测试用例/基础测试/CaseTwo/CaseTwo_ArrayNestDictViewController.swift
+++ Example/SmartCodable/测试用例/基础测试/CaseTwo/CaseTwo_ArrayNestDictViewController.swift
@@ -7,25 +7,79 @@
 //
 
 import UIKit
+import SmartCodable
 
 class CaseTwo_ArrayNestDictViewController: BaseCompatibilityViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
-        guard let dict = nullJson.toDictionary() else { return }
         
+        let arr = [
+            [
+                "name": "小明",
+                "age": 10,
+                "father": [
+                    "name": "小明的爸爸",
+                    "age": "不详",
+                    "love": NSNull()
+                ],
+                "love": [
+                    "name": NSNull(),
+//                    "time": 4
+                ],
+                "son": nil
+            ],
+            [
+                "name": "大黄",
+                "age": 20,
+                "father": [
+                    "name": "大黄的爸爸",
+                    "age": "40",
+                    "love": [
+                        "name": "打牌",
+                        "time": 20
+                    ]
+                ],
+                "love": [
+                    "name": NSNull(),
+//                    "time": 4
+                ],
+                "son": nil
+            ]
         
-        let nullArr: [Any] = [dict]
+        ]
+
         
-//        if let models = [CompatibleTypes].deserialize(array: nullArr) as? [CompatibleTypes] {
-//            print(models)
-//        }
-        
-        if let models = [OptionalCompatibleTypes].deserialize(array: nullArr) as? [OptionalCompatibleTypes] {
+        if let models = [PersonModel].deserialize(array: arr) as? [PersonModel] {
             print(models)
         }
     }
 
+}
+extension CaseTwo_ArrayNestDictViewController {
+    struct PersonModel: SmartCodable {
+        var name: String = ""
+        var age: Int = 0
+        var father: FatherModel = FatherModel()
+        var love: Love?
+        var son: SonModel?
+    }
+    
+    struct FatherModel: SmartCodable {
+        var name: String?
+        var age: Int?
+        var love: Love?
+    }
+
+    struct SonModel: SmartCodable {
+        var name: String?
+        var age: Int?
+        var love: Love?
+    }
+
+    struct Love: SmartCodable {
+        var name: String = ""
+        var time: CGFloat = 0.0
+    }
 }
--- Example/SmartCodable/测试用例/基础测试/CaseTwo/CaseTwo_DictNestArrayViewController.swift
+++ Example/SmartCodable/测试用例/基础测试/CaseTwo/CaseTwo_DictNestArrayViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SmartCodable
 
 class CaseTwo_DictNestArrayViewController: BaseCompatibilityViewController {
 
@@ -14,18 +15,62 @@ class CaseTwo_DictNestArrayViewController: BaseCompatibilityViewController {
         super.viewDidLoad()
         
 
-        guard let dict = nullJson.toDictionary() else { return }
-        
-        
-        let nullArr: [Any] = [dict]
-        
-//        if let models = [CompatibleTypes].deserialize(array: nullArr) as? [CompatibleTypes] {
-//            print(models)
-//        }
-        
-        if let models = [OptionalCompatibleTypes].deserialize(array: nullArr) as? [OptionalCompatibleTypes] {
-            print(models)
+        let dict: [String: Any] = [
+            "name": "father",
+            "age": 30,
+            "loves": [
+                [
+                    "name": "basketball",
+                    "time": 10
+                ],
+                [
+                    "name": "football",
+                    "time": 10
+                ]
+            ],
+            "sons": [
+                [
+                    "name": "son1",
+                    "age": 4,
+                    "love": [
+                        "name": "sleep",
+                        "time": 4
+                    ],
+                ],
+                [
+                    "name": "son2",
+                    "age": 4,
+                    "love": [
+                        "name": "look Books",
+                        "time": 2
+                    ],
+                ]
+            ]
+        ]
+
+       
+        if let model = FatherModel.deserialize(dict: dict) {
+            print(model)
         }
     }
 
+}
+extension CaseTwo_DictNestArrayViewController {
+    struct FatherModel: SmartCodable {
+        var name: String = ""
+        var age: Int = 0
+        var loves: [Love] = []
+        var sons: [SonModel] = []
+    }
+
+    struct SonModel: SmartCodable {
+        var name: String?
+        var age: Int?
+        var love: Love?
+    }
+
+    struct Love: SmartCodable {
+        var name: String = ""
+        var time: CGFloat = 0.0
+    }
 }
