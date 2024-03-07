59571c8ca848d425b55787da8ad61a134eed83a5
Mccc <mancong@bertadata.com>
2024-03-07 19:33:53 +0800

    修复单值容器的解码错误。

--- SmartCodable/Classes/SmartJSONDecoder/Container/_SmartJSONDecoder+SingleValueDecodingContainer.swift
+++ SmartCodable/Classes/SmartJSONDecoder/Container/_SmartJSONDecoder+SingleValueDecodingContainer.swift
@@ -28,7 +28,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     public func decode(_ type: Bool.Type) throws -> Bool {
         // 判断是否为null， 如果是null抛出异常, 交给上层keyedContainer兼容。
         try expectNonNull(Bool.self)
-        if let value = try self.unbox(storage.topContainer, as: Bool.self) {
+        if let value = try? self.unbox(storage.topContainer, as: Bool.self) {
             return value
         } else {
             return try Patcher<Bool>.defaultForType()
@@ -37,7 +37,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: Int.Type) throws -> Int {
         try expectNonNull(Int.self)
-        if let value = try self.unbox(storage.topContainer, as: Int.self) {
+        if let value = try? self.unbox(storage.topContainer, as: Int.self) {
             return value
         } else {
             return try Patcher<Int>.defaultForType()
@@ -46,7 +46,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: Int8.Type) throws -> Int8 {
         try expectNonNull(Int8.self)
-        if let value = try self.unbox(storage.topContainer, as: Int8.self) {
+        if let value = try? self.unbox(storage.topContainer, as: Int8.self) {
             return value
         } else {
             return try Patcher<Int8>.defaultForType()
@@ -55,7 +55,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: Int16.Type) throws -> Int16 {
         try expectNonNull(Int16.self)
-        if let value = try self.unbox(storage.topContainer, as: Int16.self) {
+        if let value = try? self.unbox(storage.topContainer, as: Int16.self) {
             return value
         } else {
             return try Patcher<Int16>.defaultForType()
@@ -64,7 +64,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: Int32.Type) throws -> Int32 {
         try expectNonNull(Int32.self)
-        if let value = try self.unbox(storage.topContainer, as: Int32.self) {
+        if let value = try? self.unbox(storage.topContainer, as: Int32.self) {
             return value
         } else {
             return try Patcher<Int32>.defaultForType()
@@ -73,7 +73,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: Int64.Type) throws -> Int64 {
         try expectNonNull(Int64.self)
-        if let value = try self.unbox(storage.topContainer, as: Int64.self) {
+        if let value = try? self.unbox(storage.topContainer, as: Int64.self) {
             return value
         } else {
             return try Patcher<Int64>.defaultForType()
@@ -82,7 +82,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: UInt.Type) throws -> UInt {
         try expectNonNull(UInt.self)
-        if let value = try self.unbox(storage.topContainer, as: UInt.self) {
+        if let value = try? self.unbox(storage.topContainer, as: UInt.self) {
             return value
         } else {
             return try Patcher<UInt>.defaultForType()
@@ -91,7 +91,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: UInt8.Type) throws -> UInt8 {
         try expectNonNull(UInt8.self)
-        if let value = try self.unbox(storage.topContainer, as: UInt8.self) {
+        if let value = try? self.unbox(storage.topContainer, as: UInt8.self) {
             return value
         } else {
             return try Patcher<UInt8>.defaultForType()
@@ -100,7 +100,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: UInt16.Type) throws -> UInt16 {
         try expectNonNull(UInt16.self)
-        if let value = try self.unbox(storage.topContainer, as: UInt16.self) {
+        if let value = try? self.unbox(storage.topContainer, as: UInt16.self) {
             return value
         } else {
             return try Patcher<UInt16>.defaultForType()
@@ -109,7 +109,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: UInt32.Type) throws -> UInt32 {
         try expectNonNull(UInt32.self)
-        if let value = try self.unbox(storage.topContainer, as: UInt32.self) {
+        if let value = try? self.unbox(storage.topContainer, as: UInt32.self) {
             return value
         } else {
             return try Patcher<UInt32>.defaultForType()
@@ -118,7 +118,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: UInt64.Type) throws -> UInt64 {
         try expectNonNull(UInt64.self)
-        if let value = try self.unbox(storage.topContainer, as: UInt64.self) {
+        if let value = try? self.unbox(storage.topContainer, as: UInt64.self) {
             return value
         } else {
             return try Patcher<UInt64>.defaultForType()
@@ -127,7 +127,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: Float.Type) throws -> Float {
         try expectNonNull(Float.self)
-        if let value = try self.unbox(storage.topContainer, as: Float.self) {
+        if let value = try? self.unbox(storage.topContainer, as: Float.self) {
             return value
         } else {
             return try Patcher<Float>.defaultForType()
@@ -136,7 +136,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: Double.Type) throws -> Double {
         try expectNonNull(Double.self)
-        if let value = try self.unbox(storage.topContainer, as: Double.self) {
+        if let value = try? self.unbox(storage.topContainer, as: Double.self) {
             return value
         } else {
             return try Patcher<Double>.defaultForType()
@@ -145,7 +145,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode(_ type: String.Type) throws -> String {
         try expectNonNull(String.self)
-        if let value = try self.unbox(storage.topContainer, as: String.self) {
+        if let value = try? self.unbox(storage.topContainer, as: String.self) {
             return value
         } else {
             return try Patcher<String>.defaultForType()
@@ -154,7 +154,7 @@ extension _SmartJSONDecoder : SingleValueDecodingContainer {
     
     public func decode<T : Decodable>(_ type: T.Type) throws -> T {
         try expectNonNull(T.self)
-        if let value = try self.unbox(storage.topContainer, as: T.self) {
+        if let value = try? self.unbox(storage.topContainer, as: T.self) {
             return value
         } else {
             return try Patcher<T>.defaultForType()
