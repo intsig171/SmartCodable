ab9c076b80af24bd78e234706951f25af7c16f73
Mccc <mancong@bertadata.com>
2024-04-17 19:53:23 +0800

    枚举恢复默认值

--- Example/SmartCodable/TestViewController.swift
+++ Example/SmartCodable/TestViewController.swift
@@ -14,109 +14,107 @@ import CleanJSON
 import BTPrint
 
 
+
 class TestViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         SmartConfig.debugMode = .none
-//        todo1()
-        
-        todo2()
-    }
-}
 
-
-extension TestViewController {
-    
-    func todo2() {
-        
         let dict: [String: Any] = [
-            "students": [
-                "name": "Mccc"
+            "name": "满聪的家",
+//            "type": "old",
+            "father": [
+                "name": "满聪",
+                "age": 33,
+                "sex": "man"
             ],
-            "sex": "man123"
+            "mother": [
+                "name": "刘文巧",
+                "age": 32,
+                "sex": "women",
+                "birth": "2034-12-01 18:00:00"
+            ],
+            "sons": [
+                [
+                   "name": "qiqi",
+                   "age": 3,
+                   "sex": "man",
+                   "birth": "2021-02-06"
+               ],
+                [
+                   "name": "lili",
+                   "age": 3,
+                   "sex": "man",
+                   "birth": "2021-02-06"
+               ]
+            ]
         ]
-        let model1 = ClassName.deserialize(from: dict)
-        BTPrint.print(model1)
-
-    }
-    
-    struct ClassName: SmartCodable {
-        var students: Student = Student()
-        var sex: Sex = .women
-
-        enum Sex: String, SmartCaseDefaultable {
-            static var defaultCase: ClassName.Sex = .man
-            case man
-            case women
-        }
-    }
-
-    struct Student: SmartCodable {
-        var name: String = ""
+        
+        guard let model = Family.deserialize(from: dict) else { return }
+        smartPrint(value: model)
     }
 }
 
 
-
-extension TestViewController {
+enum Sex: String, SmartCaseDefaultable {
+    static var defaultCase: Sex = .man
     
-    func todo1() {
-        let json = """
-         {
-             "matches": [
-                    {
-                        "id": 2,
-                        "time_line": [
-                             {
-                                "count": 2
-                              }
-                         ]
-                      }
-                    ]
-                }
-        """
-        
-        let model = QLMatchList.deserialize(from: json)
-        
-        BTPrint.print(model)
+    case man
+    case women
+}
 
-        if let matches = model?.matches, matches.count > 0 {
-            let uid = matches[0].id
-            if uid == 2 {
-                print("正确")
-            }else{
-                print("错误")
-            }
-        }
-    }
+enum FamilyType: String, SmartCaseDefaultable {
+    static var defaultCase: FamilyType = .new
     
+    case new
+    case old
+}
+
+
+struct Family: SmartCodable {
+    var name: String = "我的家"
+    var father = FatherModel()
+    var mother = MotherModel()
+    var sons: [SonModel] = []
+    var type: FamilyType = .new
+
+}
+
+struct FatherModel: SmartCodable {
+    var name: String = "Mccc"
+    var age: Int = 30
+    var sex: Sex = .man
+}
+
+struct MotherModel: SmartCodable {
+    var name: String = "LWQ"
+    var age: Int = 20
+    var sex: Sex = .women
+    var birth: Date?
     
-    struct QLMatchList: SmartCodable {
-        var matches: [QLMatch]?
-    }
+    static func mappingForValue() -> [SmartValueTransformer]? {
+        let dateFormatter = DateFormatter()
+         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
 
-    struct QLMatch: SmartCodable {
-        var id: Int = 0
-        var time_line: [QLTimeLine]?
-        var team_type: QLTeamType = .normal
-        
-//        enum CodingKeys: CodingKey {
-//            case id
-//            case time_line
-//        }
+        return [
+            CodingKeys.birth <--- SmartDateFormatTransformer(dateFormatter)
+        ]
     }
-    struct QLTimeLine: SmartCodable {
-        var count: Int = 2
-    }
+}
 
-    public enum QLTeamType: Int, SmartCaseDefaultable {
-        public static var defaultCase: QLTeamType = .normal
-        
-
-        case normal = 0
-        case home = 1
-        case away = 2
+struct SonModel: SmartCodable {
+    var name: String = ""
+    var age: Int = 3
+    var sex: Sex = .man
+    var birth: Date?
+    
+    static func mappingForValue() -> [SmartValueTransformer]? {
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd"
+        return [
+            CodingKeys.birth <--- SmartDateFormatTransformer(df)
+        ]
     }
 }
