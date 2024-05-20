4646c312e8e47929676b8482b128a7c54f026e7f
Mccc <mancong@bertadata.com>
2024-05-20 20:53:07 +0800

    demo

--- Example/SmartCodable/Test3ViewController.swift
+++ Example/SmartCodable/Test3ViewController.swift
@@ -16,6 +16,27 @@ class Test3ViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let json = """
+{"code":1,"data":{"index":"4","msg":"上传成功","size":1780},"msg":"success","rid":"Dg0S"}
+"""
+  
+        if let model = mapModel([String: SmartAny].self, jsonString: json) {
+            print(model)
+        }
+    }
+    
+    func mapModel<T: SmartCodable>(_ type: T.Type, jsonString: String) -> T? {
+        guard let model = type.deserialize(from:jsonString) else {
+            return nil
+        }
+        return model
+    }
+    
+    struct ResponseData<T: SmartCodable>: SmartCodable{
+        var code: Int = 0
+        var msg: String = ""
+        var data: T?
     }
 }
 
