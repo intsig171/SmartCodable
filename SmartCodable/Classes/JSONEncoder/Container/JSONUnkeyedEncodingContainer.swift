02bb193719ac51e06df739527ddf10ef74318490
Mccc <mancong@bertadata.com>
2024-06-03 17:08:59 +0800

    完成容器的分离

---
+++ SmartCodable/Classes/JSONEncoder/Container/JSONUnkeyedEncodingContainer.swift
@@ -0,0 +1,133 @@
+//
+//  JSONUnkeyedEncodingContainer.swift
+//  SmartCodable
+//
+//  Created by qixin on 2024/6/3.
+//
+
+import Foundation
+struct JSONUnkeyedEncodingContainer: UnkeyedEncodingContainer, _SpecialTreatmentEncoder {
+    let impl: JSONEncoderImpl
+    let array: JSONFuture.RefArray
+    let codingPath: [CodingKey]
+
+    var count: Int {
+        self.array.array.count
+    }
+    private var firstValueWritten: Bool = false
+    internal var options: SmartJSONEncoder._Options {
+        return self.impl.options
+    }
+
+    init(impl: JSONEncoderImpl, codingPath: [CodingKey]) {
+        self.impl = impl
+        self.array = impl.array!
+        self.codingPath = codingPath
+    }
+
+    // used for nested containers
+    init(impl: JSONEncoderImpl, array: JSONFuture.RefArray, codingPath: [CodingKey]) {
+        self.impl = impl
+        self.array = array
+        self.codingPath = codingPath
+    }
+
+    mutating func encodeNil() throws {
+        self.array.append(.null)
+    }
+
+    mutating func encode(_ value: Bool) throws {
+        self.array.append(.bool(value))
+    }
+
+    mutating func encode(_ value: String) throws {
+        self.array.append(.string(value))
+    }
+
+    mutating func encode(_ value: Double) throws {
+        try encodeFloatingPoint(value)
+    }
+
+    mutating func encode(_ value: Float) throws {
+        try encodeFloatingPoint(value)
+    }
+
+    mutating func encode(_ value: Int) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: Int8) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: Int16) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: Int32) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: Int64) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: UInt) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: UInt8) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: UInt16) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: UInt32) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode(_ value: UInt64) throws {
+        try encodeFixedWidthInteger(value)
+    }
+
+    mutating func encode<T>(_ value: T) throws where T: Encodable {
+        let key = _JSONKey(stringValue: "Index \(self.count)", intValue: self.count)
+        let encoded = try self.wrapEncodable(value, for: key)
+        self.array.append(encoded ?? .object([:]))
+    }
+
+    mutating func nestedContainer<NestedKey>(keyedBy _: NestedKey.Type) ->
+        KeyedEncodingContainer<NestedKey> where NestedKey: CodingKey
+    {
+        let newPath = self.codingPath + [_JSONKey(index: self.count)]
+        let object = self.array.appendObject()
+        let nestedContainer = JSONKeyedEncodingContainer<NestedKey>(impl: impl, object: object, codingPath: newPath)
+        return KeyedEncodingContainer(nestedContainer)
+    }
+
+    mutating func nestedUnkeyedContainer() -> UnkeyedEncodingContainer {
+        let newPath = self.codingPath + [_JSONKey(index: self.count)]
+        let array = self.array.appendArray()
+        let nestedContainer = JSONUnkeyedEncodingContainer(impl: impl, array: array, codingPath: newPath)
+        return nestedContainer
+    }
+
+    mutating func superEncoder() -> Encoder {
+        let encoder = self.getEncoder(for: _JSONKey(index: self.count))
+        self.array.append(encoder)
+        return encoder
+    }
+}
+
+extension JSONUnkeyedEncodingContainer {
+    @inline(__always) private mutating func encodeFixedWidthInteger<N: FixedWidthInteger>(_ value: N) throws {
+        self.array.append(.number(value.description))
+    }
+
+    @inline(__always) private mutating func encodeFloatingPoint<F: FloatingPoint & CustomStringConvertible>(_ float: F) throws {
+        let value = try self.wrapFloat(float, for: _JSONKey(index: self.count))
+        self.array.append(value)
+    }
+}
