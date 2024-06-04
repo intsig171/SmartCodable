7823236c57af7ac038265a8b45782969acb66d5a
Mccc <mancong@bertadata.com>
2024-06-04 14:34:03 +0800

    测试

--- Example/SmartCodable/TestViewController.swift
+++ Example/SmartCodable/TestViewController.swift
@@ -25,96 +25,61 @@ import BTPrint
 
 
 class TestViewController: BaseViewController {
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let json = """
-{
-  "typeid" : 33,
-  "data" : {
-    "boxnum" : 0,
-    "boxname" : "",
-    "queue" : 0,
-    "userinfo" : {
-      "vip" : 6,
-      "uid" : 101080,
-      "age" : 21,
-      "sex" : 1,
-      "self_cid" : 0,
-      "viplevel" : 0,
-      "color" : "#209948",
-      "avatar" : "http",
-      "sid" : "37",
-      "medal" : "http",
-      "vipicon" : "http",
-      "city" : null,
-      "teacher" : 1,
-      "nickname" : "u101080",
-      "online" : 0,
-      "name" : "u101080",
-      "vipuid" : 0
-    },
-    "gift" : {
-      "diamond" : 520,
-      "pic" : "http",
-      "title" : "魔法戒指"
-    },
-    "giftnum" : 1,
-    "self" : 0,
-    "giftUsername" : "哦公民♔⁎͜⁛",
-    "chatname" : "花开富贵HF官方",
-    "cid" : 23827
-  },
-  "cid" : 0,
-  "type" : 60
-}
-"""
+        let dict: [String: Any] = [:]
         
+        let queue = DispatchQueue.global(qos: .userInitiated)
+        let group = DispatchGroup()
         
+        queue.async(group: group) {
+            if let model = NewModel.deserialize(from: dict) {
+                //            print(model)
+            }
+        }
         
-        if let jsonObject = TOReceiveSocketItem.deserialize(from: json) {
-            print(jsonObject)
+        queue.async(group: group) {
+            print("\n\n 开始解析old")
+            
+            if let model = OldModel.deserialize(from: dict) {
+                //            print(model)
+            }
+        }
+        
+        group.notify(queue: DispatchQueue.main) {
+            print("Both methods are completed.")
         }
     }
 }
-public class TOReceiveSocketItem: SmartCodable {
-    required public init() {}
-    /// 接收socket消息类型
-    public var typeid: Int = 0
-    public var error: Int?
-    /// 错误信息
-    public var error_msg: String?
-    public var cid: Int = 0
-    public var position: Int = 0
-    public var data: [TOChatroomWheatListItem]?
-    ///  1 显示    0不显示
-    public var invite : Int?
-    public var wheatUid: [String: Int]?
-    public var uids: [Int]?
-    /// 老板
-    public var uid: Int = 0
-    /// 老板状态：0入座 -1离开
-    public var state: Int = -2
-    /// 老板消息
-    public var msg: String?
-    public var type: Int?
-    ///  你画我猜
-    public var isAllPrepared: Int? //所有人1.已准备0未准备
-    public var cd: Int? //剩余CD时间
-    public var currSignal: Int? //所处阶段
-    public var stage : Int?
-    public var desc : String?
+
+struct NewModel: SmartCodable {
+    var newa: String = ""
+    var newb: String = ""
+    var newc: String = ""
+    var newd: String = ""
+    var newe: String = ""
+    var newf: String = ""
+    var newSub: NewsubModel = NewsubModel()
 }
 
-public class TOChatroomWheatListItem: SmartCodable {
+struct NewsubModel: SmartCodable {
+    var newname: String = ""
     
-    public var boxname: String = ""
-    public var error_msg: String = ""
+}
 
-    public var boxnum: Int = 0
-    public var queue: Int = 0
-    public var cid: Int = 0
-    required public init() {}
 
+struct OldModel: SmartCodable {
+    var olda: String = ""
+    var oldb: String = ""
+    var oldc: String = ""
+    var oldd: String = ""
+    var olde: String = ""
+    var oldf: String = ""
+    var oldSub = OldsubModel()
+}
+
+struct OldsubModel: SmartCodable {
+    var oldname: String = ""
 }
