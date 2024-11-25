b4da54914981fe2296f5300bb3e9edae6de102b0
Mccc <>
2024-11-25 11:30:13 +0800

    新增测试用例

---
+++ Tests/Example.swift
@@ -0,0 +1,34 @@
+import XCTest
+import SmartCodable
+
+
+
+
+class Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+    func testBase() {
+    }
+   
+}
+
+
+
+
+
+
+// SmartCodable
+struct Smart: SmartCodable {
+    var name: String?
+
+
+
